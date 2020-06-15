1ed364b42cdc6c7613b38b6d2f294dbd3db9ccb8
Bevan Wentzel <bevan@teamgeek.co.za>
2020-06-15 16:53:26 +0200

    Populated example

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 
-import { Field, Form, Formik } from "formik";
-import { EmailField, TextField } from "formik-material-ui-elements";
+import { Field, Formik } from "formik";
+import { AutocompleteField, EmailField, PasswordField, RichTextField, SubmitButton, SwitchField, TextField } from "formik-material-ui-elements";
+import gql from "graphql-tag"
 import React from "react";
 
+import { MockedProvider } from '@apollo/react-testing';
 import { makeStyles } from '@material-ui/core/styles';
-
-import logo from "./logo.svg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,19 +15,83 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+const GET_TAGS_QUERY = gql`
+  query getAllTags(
+    $filter: String
+    $sort: [TagNodeSortEnum]
+    $before: String
+    $after: String
+    $first: Int
+    $last: Int
+  ) {
+    tags(
+      filter: $filter
+      sort: $sort
+      before: $before
+      after: $after
+      first: $first
+      last: $last
+    ) {
+      edges {
+        node {
+          name
+          id
+        }
+      }
+    }
+  }
+`
+
+const mocks = [
+  {
+    request: {
+      query: GET_TAGS_QUERY,
+      variables: {
+        filter: 'a',
+      },
+    },
+    result: {
+      data: {
+        tags: {
+          edges: [
+            { node: { id: "1", name: "Test" }},
+            { node: { id: "2", name: "Test 2" }},
+            { node: { id: "3", name: "Test 3" }}
+          ]
+        }
+      },
+    },
+  },
+];
+
 function App() {
   const classes = useStyles()
 
   return (
     <div className={classes.root}>
-      <Formik>
-          {() => (
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Formik initialValues={{}}>
+          {({ values }) => (
             <div className="form">
               <Field component={TextField} id="text" name="text" label="Text" />
               <Field component={EmailField} id="email" name="email" label="Email" />
+              <Field component={PasswordField} id="password" name="password" label="Password" />
+              <Field component={RichTextField} id="rich" name="rich" label="Rich Text" />
+              <Field component={SwitchField} id="switch" name="switch" label="Switch" />
+              <Field
+                component={AutocompleteField}
+                id="auto"
+                name="auto"
+                label="Auto"
+                connectionName="tags"
+                query={GET_TAGS_QUERY}
+                labelPath="name"
+              />
+              <SubmitButton />
             </div>
           )}
         </Formik>
+      </MockedProvider>
     </div>
   );
 }
